b2f952e792a9252a39ca48e520c3a3b2e25718de
Alo Emmanuel Temitope <emmanuelalo52@gmail.com>
2024-12-09 15:32:46 +0100

    attention and SwiGLU

--- llama-inference.py
+++ llama-inference.py
@@ -21,6 +21,12 @@ class LlamaConfig:
 
     device:str = None
 
+def repeat_kv(x, n_rep):
+    batch_size,seq_len,n_kv_heads,head_dim = x.shape
+    if n_rep == 1:
+        return x
+    else:
+        return (x[:,:,:,None,:].expand(batch_size,seq_len,n_kv_heads,n_rep,head_dim).reshape(batch_size,seq_len,n_kv_heads*n_rep,head_dim))
 
 class SelfAttention(nn.Module):
     def __init__(self,config):
@@ -33,7 +39,7 @@ class SelfAttention(nn.Module):
         self.wq = nn.Linear(config.n_emb,config.n_heads*self.head_dim,bias=False)
         self.wk = nn.Linear(config.n_emb,self.n_kv_heads*self.head_dim,bias=False)
         self.wv = nn.Linear(config.n_emb,self.n_kv_heads*self.head_dim,bias=False)
-        self.woo = nn.Linear(config.n_heads*self.head_dim,config.n_emb,bias=False)
+        self.wo = nn.Linear(config.n_heads*self.head_dim,config.n_emb,bias=False)
 
         #cache for k and v
         self.k_cache = torch.zeros((config.max_batch_size,config.max_seq_len,self.n_kv_heads,self.head_dim))
@@ -42,27 +48,43 @@ class SelfAttention(nn.Module):
     def forward(self,x,start_pos,frequency_complex):
         batch_size,seq_len,_ = x.shape
 
-        query = self.wq(x)
-        key = self.wk(x)
-        value = self.wv(x)
+        query_weight = self.wq(x)
+        key_weight = self.wk(x)
+        value_weight = self.wv(x)
 
-        query = query.view(batch_size,seq_len,self.n_head_q,self.head_dim)
-        key = key.view(batch_size,seq_len,self.n_kv_heads,self.head_dim)
+        query_weight = query_weight.view(batch_size,seq_len,self.n_head_q,self.head_dim)
+        key_weight = key_weight.view(batch_size,seq_len,self.n_kv_heads,self.head_dim)
 
-        value = value.view(batch_size,seq_len,self.n_kv_heads,self.head_dim)
+        value_weight = value_weight.view(batch_size,seq_len,self.n_kv_heads,self.head_dim)
 
         #apply RoPE
-        query = rotatry_postional_embedding(query,frequency_complex,device=x.device)
-        key = rotatry_postional_embedding(key,frequency_complex,device=x.device)
+        query_weight = rotatry_postional_embedding(query_weight,frequency_complex,device=x.device)
+        key_weight = rotatry_postional_embedding(key_weight,frequency_complex,device=x.device)
 
         #append the position of kv cache
-        self.k_cache[:batch_size,start_pos:start_pos+seq_len] = key
-        self.v_cache[:batch_size,start_pos:start_pos+seq_len] = value
+        self.k_cache[:batch_size,start_pos:start_pos+seq_len] = key_weight
+        self.v_cache[:batch_size,start_pos:start_pos+seq_len] = value_weight
 
         #retrieve the tokens into the kv cache
         keys = self.k_cache[:batch_size,0:start_pos+seq_len]
         value = self.k_cache[:batch_size,0:start_pos+seq_len]
+        
+        #broadcast through the entire head
+        keys = repeat_kv(keys,self.r_rep)
+        value = repeat_kv(value,self.r_rep)
 
+        query_weight = query_weight.transpose(1,2)
+        keys = key_weight.transpose(1,2)
+        value = value_weight.transpose(1,2)
+
+        #attention weight
+        scores = torch.matmul(query_weight,keys.transpose(2,3))/math.sqrt(self.head_dim)
+        scores = F.softmax(scores.float(),dim=-1).type_as(query_weight)
+
+        output = torch.matmul(scores,value)
+        output = (output.transpose(1,2).contiguous().view(batch_size,seq_len,-1))
+
+        return self.wo(output)
 
 def compute_theta_pos_freq(head_dim,seq_len,device,theta=1000.0):
     assert head_dim % 2 ==0
@@ -107,6 +129,23 @@ class RMSNorm(nn.Module):
         return x * torch.rsqrt(x.pow(2).mean(-1,keepdim=True)+self.eps)
     def forward(self,x):
         return self.gamma * self._norm(x.float()).type_as(x)
+    
+class FeedForward(nn.Module):
+    def __init__(self,config):
+        super().__init__()
+        hidden_dim = 4 * config.n_emb
+        hidden_dim = int(2 * hidden_dim / 3)
+        if config.multiple_of is not None:
+            hidden_dim = config.multiple_of *((hidden_dim + config.multiple_of - 1) // config.multiple_of)
+        self.w1 = nn.Linear(config.n_emb, hidden_dim,bias=False)
+        self.w2 = nn.Linear(hidden_dim, config.n_emb, bias=False)
+        self.w3 = nn.Linear(config.n_emb, hidden_dim, bias=False)
+    def forward(self,x):
+        swish = F.silu(self.w1(x))
+        x_v = self.w3(x)
+        x = swish * x_v
+        x = self.w2(x)
+        return x
 class LLamaTransformer(nn.Module):
     def __init__(self,config):
         super().__init__()
